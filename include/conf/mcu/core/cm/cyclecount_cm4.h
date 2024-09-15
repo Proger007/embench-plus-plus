1baa2bf5437f5b63f0af4f4d415523c0b95f7837
Alexey Karelin <alexey@karelin.dev>
2024-09-15 21:27:40 +0200

    Added cortex-m4 cycne counter

---
+++ include/conf/mcu/core/cm/cyclecount_cm4.h
@@ -0,0 +1,31 @@
+//
+// Created by Alexey on 15.09.2024.
+//
+
+#ifndef EMBENCH_INCLUDE_CONF_MCU_CORE_CM_CYCLECOUNT_CM_H
+#define EMBENCH_INCLUDE_CONF_MCU_CORE_CM_CYCLECOUNT_CM_H
+
+#include "core_cm4.h"
+
+namespace embpp::conf::mcu::core::cm4 {
+
+class CycleCounter
+    {
+public:
+    inline void setup()
+    {
+        if (!(CoreDebug->DEMCR & CoreDebug_DEMCR_TRCENA_Msk)) {
+            CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+        }
+        DWT->CYCCNT = 0;
+        DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
+    }
+    inline uint32_t get_metric() const
+    {
+        return DWT->CYCCNT;
+    }
+};
+
+}  // namespace embpp::conf::mcu::core::cm
+
+#endif //EMBENCH_INCLUDE_CONF_MCU_CORE_CM_CYCLECOUNT_CM_H
